6245b9809945756733d879e0ef649835fa422939
Gage Howe <howe.gaged@gmail.com>
2025-05-15 19:44:04 -0500

    solid progress

--- powpiler/main.go
+++ powpiler/main.go
@@ -1,5 +1,10 @@
 package main
 
+// https://www.aaronraff.dev/blog/how-to-write-a-lexer-in-go
+// https://boyter.org/posts/faster-literal-string-matching-in-go/
+
+// No LLMS were involved in the making of this program o7
+
 import (
 	"bufio"
 	"fmt"
@@ -42,7 +47,7 @@ var bash_tokens = []string{
 }
 
 var powershell_tokens = []string{
-	T_NEWLINE: "`n",
+	T_NEWLINE: "\n",
 	// todo
 }
 
@@ -81,43 +86,53 @@ func main() {
 // takes file and returns
 func compile(f *os.File) (string, error) {
 	reader := bufio.NewReader(f)
-	// outTokens := make([]FilledToken, 0)
+	lines, _ := FileToSlice(*reader, f)
 
-	// fast file->memory reader; make slice of lines
-	outString := ""
-	for {
-		line, _, err := reader.ReadLine()
-		if err != nil {
-			break // EOF
+	currChar := 0
+
+	tokens := make([]FilledToken, 0)
+	for _, v := range lines {
+		for {
+			idx := multiIndex(v, bash_tokens)
+			tokens = append(tokens, FilledToken{typ: T_EOF, val: string(v[idx])})
+			fmt.Printf("idx: %d\n", idx)
+			currChar += idx + 1
 		}
-
-		outString += string(line) + "\n"
 	}
 
-	lines := strings.Split(string(outString), "\n")
-
-	// // lexer
-	// char := 0
-	// line := 0
-	// buf := ""
-	// outTokens := make([]FilledToken, 0)
-	// for {
-	// 	switch lines[char] {
-	// 	case '#':
-	// 		line++ // comment, ignore rest of line
-	// 	case '\n':
-	// 		outTokens = append(outTokens, FilledToken{T_NEWLINE, "\n"})
-	// 	default:
-	// 		// no recognized token
-	// 		buf += string(outString[char])
-	// 	}
-	// 	char++
-	// }
-
-	// iterate over outLines
+	// print original input file
 	for i := 0; i < len(lines); i++ {
 		fmt.Printf("%s\n", lines[i])
 	}
 
 	return "", nil
+}
+
+// read a file to slice of strings
+func FileToSlice(r bufio.Reader, f *os.File) ([]string, error) {
+	outString := ""
+	for {
+		line, _, err := r.ReadLine()
+		if err != nil {
+			break // EOF
+		}
+		outString += string(line) + "\n"
+	}
+	lines := strings.Split(string(outString), "\n")
+	return lines, nil
+}
+
+// read until a target substring string is found
+// return index
+// serious leetcode flashbacks
+func multiIndex(s string, targets []string) int {
+	for i := 0; i < len(s); i++ { // for each letter s[i]
+		for j := 0; j < len(targets); j++ { // iterate over target targets[j]
+			foundIdx := strings.Index(s[i:], targets[j])
+			if foundIdx != -1 {
+				return foundIdx + i
+			}
+		}
+	}
+	return -1
 }
